34d817607d76f92091d9ae2a64c8f2604c215020
Martin Sebastian <martin-sebastian@users.noreply.github.com>
2023-11-02 16:33:59 -0400

    Image Slider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <script>
-	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	let images = [];
+
+	fetch('https://newportal.flatoutmotorcycles.com/portal/public/api/majorunit/stocknumber/P25060')
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.Images && Array.isArray(data.Images)) {
+				images = data.Images;
+			}
+		});
 
 	// Components
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { CodeBlock, LightSwitch, Tab, TabGroup } from '@skeletonlabs/skeleton';
 </script>
 
@@ -27,10 +36,12 @@
 		</h4>
 
 		<div
-			class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10"
+			class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-0 py-1"
 		>
-			{#each Array.from({ length: 8 }) as _, i}
-				<div class="snap-start shrink-0 card py-20 w-40 md:w-80 text-center">{i + 1}</div>
+			{#each images as image, index (index)}
+				<img class="rounded-xl" src={image.ImgURL} alt={`Image ${index + 1}`} />
+			{:else}
+				<p>Loading...</p>
 			{/each}
 		</div>
 
@@ -44,16 +55,18 @@
 		<hr />
 		<h4 class="h4">Built With These Technologies</h4>
 		<section>
-			<a class="btn variant-filled-primary" href="https://kit.svelte.dev/" target="_blank"
+			<a class="btn variant-filled-primary mb-1" href="https://kit.svelte.dev/" target="_blank"
 				>SvelteKit</a
 			>
-			<a class="btn variant-filled-primary" href="https://tailwindcss.com/" target="_blank"
+			<a class="btn variant-filled-primary mb-1" href="https://tailwindcss.com/" target="_blank"
 				>Tailwind</a
 			>
-			<a class="btn variant-filled-primary" href="https://www.skeleton.dev/" target="_blank"
+			<a class="btn variant-filled-primary mb-1" href="https://www.skeleton.dev/" target="_blank"
 				>Skeleton</a
 			>
-			<a class="btn variant-filled-primary" href="https://github.com/" target="_blank">GitHub</a>
+			<a class="btn variant-filled-primary mb-1" href="https://github.com/" target="_blank"
+				>GitHub</a
+			>
 		</section>
 		<hr />
 		<p>
@@ -63,35 +76,35 @@
 		</p>
 		<span
 			class="inline-flex items-center rounded-md bg-gray-500 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
-			>Badge</span
+			>VS Code</span
 		>
 		<span
 			class="inline-flex items-center rounded-md bg-red-500 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
-			>Badge</span
+			>HTML</span
 		>
 		<span
 			class="inline-flex items-center rounded-md bg-yellow-500 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"
-			>Badge</span
+			>JavaScript</span
 		>
 		<span
 			class="inline-flex items-center rounded-md bg-green-500 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
-			>Badge</span
+			>CSS</span
 		>
 		<span
 			class="inline-flex items-center rounded-md bg-blue-500 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10"
-			>Badge</span
+			>SvelteKit</span
 		>
 		<span
 			class="inline-flex items-center rounded-md bg-indigo-500 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10"
-			>Badge</span
+			>Skeleton</span
 		>
 		<span
 			class="inline-flex items-center rounded-md bg-purple-500 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10"
-			>Badge</span
+			>Tailwind</span
 		>
 		<span
 			class="inline-flex items-center rounded-md bg-pink-500 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10"
-			>Badge</span
+			>Vercel</span
 		>
 	</div>
 </main>
