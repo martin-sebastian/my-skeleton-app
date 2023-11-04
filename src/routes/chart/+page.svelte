5c7f210d545371078b70bf94785d195b02f7d658
Martin Sebastian <martin@martinsebastian.co>
2023-11-04 16:33:23 -0400

    Svg Skull Fix

--- src/routes/chart/+page.svelte
+++ src/routes/chart/+page.svelte
@@ -1,6 +1,10 @@
-<main class="">
+<script>
+	import svgSkull from '$lib/images/svg-skull.svg';
+</script>
+
+<main class="p-8">
 	<h1 class="h1 mb-5">Chart</h1>
 	<div class="container h-full mx-auto flex justify-center items-center">
-		<img class="w-96" src="/src/lib/images/svg-skull.svg" alt="" />
+		<img class="w-1/2" src={svgSkull} alt="Skull svg graphic AI generated" />
 	</div>
 </main>
