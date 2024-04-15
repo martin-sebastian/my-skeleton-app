bbc53f22c8d9964b52f7aaac55d5faa071912874
Martin Sebastian <martin-sebastian@users.noreply.github.com>
2024-04-15 16:00:35 -0400

    added await

---
+++ src/routes/vehiclepage/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+	import { onMount } from 'svelte';
+	import apiData from './stores.js';
+	import { ProgressBar } from '@skeletonlabs/skeleton';
+
+	let data = {};
+
+	onMount(async () => {
+		// Wait for the data to be fetched
+		data = $apiData;
+		console.log(data);
+	});
+</script>
+
+<main class="p-8">
+	<h1 class="h1 mb-10">Vehicle Data via API</h1>
+
+	{#await $apiData}
+		<h5 class="h5 mb-2 font-semibold">Loading</h5>
+		<ProgressBar value={undefined} />
+	{:then data}
+		<div class="max-w-xl rounded-xl overflow-hidden shadow-lg">
+			<img class="w-full" src={data.ImageUrl} alt="{data.ModelName} - {data.Color}" />
+			<div class="px-6 py-4">
+				<div class="font-bold text-xl mb-2">
+					{data.vehicleTitle}
+				</div>
+				<p class="text-gray-500 text-base">
+					{data.MSRP}
+				</p>
+				<p>{data.formattedDate}</p>
+			</div>
+			<div class="grid grid-flow-col grid-rows-1 grid-cols-5 gap-2">
+				<div>
+					<img src={data.ImageUrl} alt="" loading="lazy" />
+				</div>
+				<div>
+					<img src={data.ImageUrl} alt="" loading="lazy" />
+				</div>
+				<div>
+					<img src={data.ImageUrl} alt="" loading="lazy" />
+				</div>
+				<div>
+					<img src={data.ImageUrl} alt="" loading="lazy" />
+				</div>
+				<div>
+					<img src={data.ImageUrl} alt="" loading="lazy" />
+				</div>
+			</div>
+			<div class="px-6 pt-4 pb-2">
+				<span
+					class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+					>{data.StockNumber}</span
+				>
+				<span
+					class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+					>{data.ModelCode}</span
+				>
+				<span
+					class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+					>{data.B50ModelTypeStyle}</span
+				>
+				{#if data.Stocked === true}
+					<span
+						class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+						>In Stock</span
+					>
+				{:else}
+					<span
+						class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+						>Not In Stock</span
+					>
+				{/if}
+			</div>
+		</div>
+		<div class="w-5 h-36" />
+		<hr />
+		<pre class="bg-gray-900">{JSON.stringify(data, null, 2)}</pre>
+	{:catch error}
+		<p>{error.message}</p>
+	{/await}
+</main>
---
+++ src/routes/vehiclepage/stores.js
@@ -0,0 +1,31 @@
+import { writable } from 'svelte/store';
+
+// Create a writable store with initial value of null
+const store = writable(null);
+
+// Function to fetch data from the API
+async function fetchData() {
+	const response = await fetch(
+		'https://newportal.flatoutmotorcycles.com/portal/public/api/majorunit/stocknumber/P25060'
+	);
+	const data = await response.json();
+
+	// Create vehicleTitle and expirationDate properties
+	const vehicleTitle = `${data.ModelYear} ${data.Manufacturer} ${data.ModelName} ${data.Color}`;
+	const expirationDate = new Date(data.ExpirationDate);
+
+	// Format the date
+	const formattedDate = expirationDate.toLocaleDateString('en-US', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	});
+
+	// Update the store with the fetched data
+	store.set({ ...data, vehicleTitle, expirationDate, formattedDate });
+}
+
+// Fetch data when the store is created
+fetchData();
+
+export default store;
