fbd9f90433135f9a035cf73b948c22ce44b04f87
Martin Sebastian <martin-sebastian@users.noreply.github.com>
2024-08-05 11:45:46 -0400

    Parts API CALL

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -100,6 +100,19 @@
 			</a>
 		</li>
 		<li class="hover:variant-filled-primary hover:text-white rounded-lg">
+			<a href="/parts" on:click={drawerClose}>
+				<svg
+					class="fill-current w-4 h-4 mr-3"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					><path
+						d="M15 9h-3v2h3v3h2v-3h3V9h-3V6h-2v3zM0 3h10v2H0V3zm0 8h10v2H0v-2zm0-4h10v2H0V7zm0 8h10v2H0v-2z"
+					/></svg
+				>
+				Parts
+			</a>
+		</li>
+		<li class="hover:variant-filled-primary hover:text-white rounded-lg">
 			<a href="/vehicle" on:click={drawerClose}>
 				<svg
 					class="fill-current w-4 h-4 mr-3"
